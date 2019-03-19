f5b60276f3523ce6735defd3cd5190381fcb12b2
ShivankSahai <shivank.sahai2017@vitstudent.ac.in>
2019-03-19 16:39:57 +0530

    some

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 
 app.get('/users', (req, res) => {
     User.find().then((users) => {
-        res.send({ users })
+        res.send(users)
     }).catch((err) => {
         res.status(400).send(err)
     })
